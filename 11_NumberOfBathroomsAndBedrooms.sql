e70bc5762621daa7f3a950b9f0f8952ec0cf7342
XuanKhanh Nguyen <ngxuankhanhdl@gmail.com>
2022-01-24 20:20:27 -0500

    finish day 12

---
+++ 11_NumberOfBathroomsAndBedrooms.sql
@@ -0,0 +1,37 @@
+-- Stratascratch
+-- Airbnb Interview Questions
+/*
+Number Of Bathrooms And Bedrooms
+Find the average number of bathrooms and bedrooms for each city’s property types. 
+Output the result along with the city name and the property type.
+
+Table: airbnb_search_details
+
+id                              int
+price                           float
+property_type                   varchar
+room_type                       varchar
+amenities                       varchar
+accommodates                    int
+bathrooms                       int
+bed_type                        varchar
+cancellation_policy             varchar
+cleaning_fee                    bool
+city                            varchar
+host_identity_verified          varchar
+host_response_rate              varchar
+host_since                      datetime
+neighbourhood                   varchar
+number_of_reviews               int
+review_scores_rating            float
+zipcode                         int
+bedrooms                        int
+beds                            int
+*/
+
+select city,
+        property_type,  
+        avg(bedrooms)as average_bedrooms,
+        avg(bathrooms) as average_bathrooms
+from airbnb_search_details
+group by city, property_type;
---
+++ 12_PopularityOfHack.sql
@@ -0,0 +1,36 @@
+-- Stratascratch
+-- Meta Interview Questions
+
+/*
+Meta/Facebook has developed a new programing language called Hack.
+To measure the popularity of Hack they ran a survey with their employees. 
+The survey included data on previous programing familiarity 
+as well as the number of years of experience, age, 
+gender and most importantly satisfaction with Hack. 
+Due to an error location data was not collected, 
+but your supervisor demands a report showing average popularity of Hack by office location. 
+Luckily the user IDs of employees completing the surveys were stored.
+Based on the above, find the average popularity of the Hack per office location.
+Output the location along with the average popularity.
+
+Tables: facebook_employees, facebook_hack_survey
+
+facebook_employees
+id                  int
+location            varchar
+age                 int
+gender              varchar
+is_senior           bool
+
+facebook_hack_survey
+employee_id         int
+age                 int
+gender              varchar
+popularity          int
+
+
+select location, avg(popularity) as average_popularity
+from facebook_employees
+left join facebook_hack_survey
+on facebook_hack_survey.employee_id = facebook_employees.id
+group by location;
