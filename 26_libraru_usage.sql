432f72874fbbb9959ccda1827662a27037aff69d
XuanKhanh Nguyen <ngxuankhanhdl@gmail.com>
2022-02-10 06:51:04 -0500

    complete day 26

---
+++ 26_libraru_usage.sql
@@ -0,0 +1,47 @@
+-- Stratascratch
+-- City of San Francisco Practice Questions
+
+/* 
+Find libraries who haven't provided the email address in 2016 but their notice preference definition is set to email.
+Output the library code.
+
+Table: library_usage
+
++-----------------------------+------------+
+| patron_type_code            | int        |   
++-----------------------------+------------+
+| patron_type_definition      | varchar    |
++-----------------------------+------------+
+| total_checkouts             | int        |
++-----------------------------+------------+
+| total_renewals              | int        |
++-----------------------------+------------+
+| age_range                   | varchar    |
++-----------------------------+------------+
+| home_library_code           | varchar    |
++-----------------------------+------------+
+| home_library_definition     | varchar    |
++-----------------------------+------------+
+| circulation_active_month    | varchar    |
++-----------------------------+------------+
+| circulation_active_year     | float      |
++-----------------------------+------------+
+| notice_preference_code      | varchar    |
++-----------------------------+------------+
+| notice_preference_definition| varchar    |
++-----------------------------+------------+
+| provided_email_address      | bool       |
++-----------------------------+------------+
+| year_patron_registere       | dint       |
++-----------------------------+------------+
+| outside_of_county           | bool       |
++-----------------------------+------------+
+| supervisor_district         | float      |
++-----------------------------+------------+
+*/
+
+SELECT home_library_code
+FROM library_usage
+WHERE circulation_active_year = '2016'
+AND notice_preference_definition = 'email'
+AND provided_email_address = 'FALSE';
