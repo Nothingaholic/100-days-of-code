22df9c30e16c63c1fb7c41cb7585857c527363d7
XuanKhanh Nguyen <ngxuankhanhdl@gmail.com>
2022-02-11 06:54:32 -0500

    complete day 28

--- 28_fbWebLog.sql
+++ 28_fbWebLog.sql
@@ -19,3 +19,19 @@ Table: facebook_web_log
 
 
 */
+
+
+SELECT user_id, avg(session) AS "average_session_time"
+FROM 
+( SELECT t1.user_id, 
+        date(t1.timestamp) AS dt,
+        min(t2.timestamp) - max(t1.timestamp) AS session
+    from facebook_web_log t1
+    LEFT JOIN facebook_web_log t2
+    ON t1.user_id = t2.user_id
+    WHERE t1.action = 'page_load'
+    AND t2.action = 'page_exit'
+    AND date(t1.timestamp) = date(t2.timestamp)
+    GROUP BY t1.user_id, date(t1.timestamp)
+) a 
+GROUP BY user_id;
