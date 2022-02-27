e288f297f3277142b3ff7099a6ece7059053567f
XuanKhanh Nguyen <ngxuankhanhdl@gmail.com>
2022-02-27 06:54:16 -0500

    complete day 40

--- 40_SQLexercises.sql
+++ 40_SQLexercises.sql
@@ -8,9 +8,30 @@ FROM customers c, orders o, orderdetails od
 WHERE c.customernumber = o.customernumber
 AND o.ordernumber = od.ordernumber
 AND od.priceeach <= 70;
+
+
+SELECT customername
+FROM customers c
+JOIN orders o ON c.customernumber = o.customernumber -- customers who made orders
+WHERE o.customernumber in ( SELECT c.customernumber
+			FROM customers c, orders o, orderdetails od, products p
+			WHERE c.customernumber = o.customernumber
+			AND o.ordernumber = od.ordernumber
+			AND od.productcode = p.productcode
+			AND buyprice NOT IN (SELECT buyprice
+							FROM products 
+							WHERE buyprice >= 70
+						 )
+						);
+                        
 /*
 10. Find the number of sales made by each employee, 
 including those who have not made any sales (e.g. the managers, the president, etc.). 
 The results of your query should be of the form (employee ID, count of sales made). 
 Employees who have never made a sale should have an associated count of zero.
 */
+SELECT e.employeenumber, COUNT(o.customernumber) as " count of sales made"
+FROM employees e
+LEFT JOIN customers c ON c.salesrepemployeenumber = e.employeenumber
+LEFT JOIN orders o ON o.customernumber = c.customernumber
+GROUP BY employeenumber;
