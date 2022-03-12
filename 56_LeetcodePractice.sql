8f31a5d2bd3852520e4704e322222d80048a82e1
XuanKhanh Nguyen <ngxuankhanhdl@gmail.com>
2022-03-11 21:18:00 -0500

    update day56

---
+++ 56_LeetcodePractice.sql
@@ -0,0 +1,41 @@
+-- Combine 2 table
+-- https://leetcode.com/problems/combine-two-tables/
+-- Write an SQL query to report the first name, last name, city, and state of each person in the Person table. 
+-- If the address of a personId is not present in the Address table, report null instead.
+
+SELECT firstname, lastname, city, state
+FROM person p
+LEFT JOIN address a
+ON p.personId= a.personId
+
+
+
+-- Write an SQL query to find the employees who earn more than their managers.
+-- Return the result table in any order.
+-- https://leetcode.com/problems/employees-earning-more-than-their-managers/
+SELECT e1.name as employee
+FROM employee e1, employee e2
+WHERE e1.managerId = e2.id
+AND e1.salary > e2.salary
+
+
+-- Duplicate Emails
+-- Write an SQL query to report all the duplicate emails.
+-- https://leetcode.com/problems/duplicate-emails/
+SELECT email
+FROM person p1
+GROUP BY email
+HAVING count(email) > 1
+
+
+-- Customers Who Never Order
+-- https://leetcode.com/problems/duplicate-emails/
+
+SELECT name as Customers
+FROM customers c
+WHERE c.id NOT IN (SELECT customerId
+                  FROM orders)
+
+-- Delete Duplicate Emails
+-- https://leetcode.com/problems/delete-duplicate-emails/
+
